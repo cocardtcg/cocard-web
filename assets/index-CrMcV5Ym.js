f6063a8225a3e1e77c64a5e41e478280a8b7a6cf
cocardtcg <cocardtcg@users.noreply.github.com>
2026-04-13 01:07:29 +0900

    v1.0.13

--- assets/index-CrMcV5Ym.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BO_3Bkcq.js","assets/index-Cn7k7Fld.js","assets/index-D8bavZWs.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-Cn7k7Fld.js";const _=p("App",{web:()=>r(()=>import("./web-BO_3Bkcq.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

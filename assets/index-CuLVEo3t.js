11d3cc04a324e40ca4632c23b7ffdaa355261902
cocardtcg <cocardtcg@users.noreply.github.com>
2026-06-28 05:48:50 +0900

    v1.0.23

---
+++ assets/index-CuLVEo3t.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BufU9cCo.js","assets/index-BvOV1z5e.js","assets/index-5pzHZUMs.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-BvOV1z5e.js";const _=p("App",{web:()=>r(()=>import("./web-BufU9cCo.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

b3707da68cff0b81a35c31c4f06bbd5f0625c669
cocardtcg <cocardtcg@users.noreply.github.com>
2026-06-28 06:02:00 +0900

    v1.0.25

--- assets/index-C3PmXhtv.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Dd47b3d0.js","assets/index-DAV4YIiu.js","assets/index-5pzHZUMs.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-DAV4YIiu.js";const _=p("App",{web:()=>r(()=>import("./web-Dd47b3d0.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

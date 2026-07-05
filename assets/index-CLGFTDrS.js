0cd9b79e6cb9df90414d2bcb96e13adc1ab96fbf
cocardtcg <cocardtcg@users.noreply.github.com>
2026-07-05 23:53:02 +0900

    v1.0.28

--- assets/index-CLGFTDrS.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-6UYjyBwe.js","assets/index-Bvr9mXW1.js","assets/index-HW4f8nwk.css"])))=>i.map(i=>d[i]);
-import{r as p,_ as r}from"./index-Bvr9mXW1.js";const _=p("App",{web:()=>r(()=>import("./web-6UYjyBwe.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

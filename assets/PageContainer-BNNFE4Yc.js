fbcfea53a9462b82ef5f0b46963f5b1a8ff005a6
cocardtcg <cocardtcg@users.noreply.github.com>
2026-07-23 21:44:46 +0900

    v1.1.18

---
+++ assets/PageContainer-BNNFE4Yc.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-DKyFSxg0.js";function r({children:a,className:e=""}){return o.jsx("main",{className:`min-h-0 flex-1 overflow-y-auto pb-safe ${e}`,children:a})}export{r as P};
---
+++ assets/index-I6Gt4cJO.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-BvSQ6oyS.js","assets/index-DKyFSxg0.js","assets/index-Dkz5Nou_.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-DKyFSxg0.js";const _=p("App",{web:()=>r(()=>import("./web-BvSQ6oyS.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

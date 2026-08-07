1e4e97a8260d9b5af983712b098fabc2837feae8
cocardtcg <cocardtcg@users.noreply.github.com>
2026-08-07 12:58:32 +0900

    v1.1.21

---
+++ assets/PageContainer-DO6XagJS.js
@@ -0,0 +1,1 @@
+import{j as o}from"./index-C21MfStV.js";function r({children:a,className:e=""}){return o.jsx("main",{className:`min-h-0 flex-1 overflow-y-auto pb-safe ${e}`,children:a})}export{r as P};

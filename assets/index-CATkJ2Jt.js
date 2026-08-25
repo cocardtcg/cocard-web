ddb0072a0c16d801ed8a2dd6976a8a61647a6d63
cocardtcg <cocardtcg@users.noreply.github.com>
2026-08-25 20:52:02 +0900

    v1.1.24

---
+++ assets/index-CATkJ2Jt.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CyrBIs7n.js","assets/index-C28fwv2o.js","assets/index-Bg8-TW-P.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-C28fwv2o.js";const _=p("App",{web:()=>r(()=>import("./web-CyrBIs7n.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};

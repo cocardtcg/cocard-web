3cc230a33c7780c7e9f887698f057151699ea574
cocardtcg <cocardtcg@users.noreply.github.com>
2026-09-06 14:52:46 +0900

    v1.1.25

---
+++ assets/index-Bj3y6pta.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-DyfPYpWz.js","assets/index-CRH-ZDOh.js","assets/index-P3KtF1R7.css"])))=>i.map(i=>d[i]);
+import{r as p,_ as r}from"./index-CRH-ZDOh.js";const _=p("App",{web:()=>r(()=>import("./web-DyfPYpWz.js"),__vite__mapDeps([0,1,2])).then(e=>new e.AppWeb)});export{_ as App};
